082501d1c3a073cf5ee837d7b4e6914cf1ba5e7b
m988ar <vborja2003@gmail.com>
2026-06-12 16:53:43 -0300

    punto 1 terminado

--- LectorDirectorio/Directorio.cs
+++ LectorDirectorio/Directorio.cs
@@ -3,9 +3,28 @@ class Directorio
 {
     public static void existe(string carpeta)
     {
-        if(Directory.Exists(carpeta))
+        if (Directory.Exists(carpeta))
         {
-            System.Console.WriteLine("COSAS");
+            System.Console.WriteLine("Carpetas:");
+            foreach (string nombreCarpeta in Directory.GetDirectories(carpeta))
+            {
+                System.Console.WriteLine($"_{nombreCarpeta.Split('\\').LastOrDefault()}");
+            }
+            System.Console.WriteLine("Archivos:");
+            foreach (string NombreArchivo in Directory.GetFiles(carpeta))
+            {
+                FileInfo archivo = new FileInfo(NombreArchivo);
+                System.Console.WriteLine($"_{archivo.Name} {archivo.Length / 1000}kb");
+            }
+            using (StreamWriter reporte = new StreamWriter(System.IO.Path.Combine(carpeta, "reporte_archivos.csv")))
+            {
+                reporte.WriteLine("Nombre,Tamanio,Ultima Modificacion");
+                foreach (string NombreArchivo in Directory.GetFiles(carpeta))
+                {
+                    FileInfo archivo = new FileInfo(NombreArchivo);
+                    reporte.WriteLine($"{archivo.Name},{Math.Round((double)(archivo.Length / 1000),2)}KB,{archivo.CreationTime}");
+                }
+            }
         }
         else
         {
